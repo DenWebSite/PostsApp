a88d4b10a68b534ca421c822af32d1786c3322ae
Denis <evgenjuk12@gmail.com>
2026-09-10 10:36:58 +0300

    реализовал добавление поста и его рендер

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ function App() {
   const storePost = (e) => {
     e.preventDefault();
 
-    setPosts([...posts, post])
+    setPosts([...posts, post]);
   };
 
   const [posts, setPosts] = useState([]);
 
   return (
     <div className="bg-gray-500 min-h-screen p-4">
-      <div className="bg-white w-1/2 mx-auto p-4 border border-gray-200">
+      <div className="bg-white w-1/2 mx-auto mb-4 p-4 border border-gray-200">
         <div className="mb-4">
           <input
             onChange={(e) => handlePost(e)}
@@ -53,6 +53,13 @@ function App() {
           </a>
         </div>
       </div>
+
+      {posts.map((post) => (
+        <div key={post.title+post.content} className="bg-white w-1/2 mx-auto mb-4 p-4 border border-gray-200">
+          <h3 className="title text-lg mb-2">{post.title}</h3>
+          <p className="text-xs">{post.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
